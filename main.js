0133ce2167f653fefc9fd1ce96bc010fb5022185
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2020-12-02 19:57:53 +0100

    main.js was created

---
+++ main.js
@@ -0,0 +1,29 @@
+let customerRank = 3;
+let brand = "VW";
+let color = "Siyah";
+let model = 2030;
+
+
+if (customerRank >= 5) {
+  console.log("Kaputu Ac Abi!");
+} else if (
+  (brand === "BMW" && color === "Kirmizi" && model >= 2010 && model <= 2019) ||
+  (color === "Siyah" && model >= 2020)
+) {
+  console.log("Kaputu Ac Abi!");
+} else if (
+  brand === "Audi" &&
+  (color === "Kirmizi" || color === "Siyah") &&
+  ((model <= 2010 && model >= 2005) || (model <= 2014 && model >= 2010))
+) {
+  console.log("Kaputu Ac Abi!");
+} else if (
+  brand === "VW" &&
+  color === "Siyah" &&
+  model <= 2018 &&
+  model >= 2001
+) {
+  console.log("Kaputu Ac Abi!")
+} else {
+  console.log("Biz Bakmiyoruz, Ali Usta Baksin!")
+}
